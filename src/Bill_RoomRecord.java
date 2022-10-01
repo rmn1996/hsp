69d90dbb538917b1212060dd2328926b3b5a7bf1
raman199618 <40624625+raman199618@users.noreply.github.com>
2022-10-01 15:28:03 +0530

    Update Bill_RoomRecord.java

--- src/Bill_RoomRecord.java
+++ src/Bill_RoomRecord.java
@@ -164,6 +164,17 @@ PreparedStatement pst=null;
        frm.setVisible(true);
     }//GEN-LAST:event_formWindowClosing
 
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
     /**
      * @param args the command line arguments
      */
